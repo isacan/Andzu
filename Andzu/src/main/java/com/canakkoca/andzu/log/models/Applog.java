1e8d045c3c76a22a7875bc95f32791220fcbfb30
isa can akkoca <isacan_akkoca@hotmail.com>
2017-04-14 15:13:52 +0300

    duzenleme AppLog

--- Andzu/src/main/java/com/canakkoca/andzu/log/models/Applog.java
+++ Andzu/src/main/java/com/canakkoca/andzu/log/models/Applog.java
@@ -1,6 +1,5 @@
 package com.canakkoca.andzu.log.models;
 
-import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 
 import java.io.Serializable;
@@ -9,12 +8,54 @@ import java.io.Serializable;
  * Created by can.akkoca on 4/13/2017.
  */
 
-@Entity
-public class Applog implements Serializable {
+
+public class AppLog implements Serializable {
 
     private static final long serialVersionUID = 12341L;
 
-    @Id
-    private Long id;
+    private int id;
+    private String priority;
+    private String tag;
+    private String message;
+    private int created_at;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(int created_at) {
+        this.created_at = created_at;
+    }
 }
